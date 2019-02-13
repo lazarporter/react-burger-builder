07a86ed30a3554959ca5ecf0e3145bf3c45c5bbb
Lazer Porter <lazar.porter@gmail.com>
2019-02-12 23:00:31 -0500

    added radium, inline styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Radium, {StyleRoot} from 'radium'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -19,8 +19,7 @@ class App extends Component {
    this.setState({
      persons: personsTemp
    })
-   
- }
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex( prsn => {
@@ -43,11 +42,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
     
     let persons = null
@@ -66,21 +70,36 @@ class App extends Component {
           })}
       </div>
       )
+      style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
     
+    const classes = []
+    if (this.state.persons.length <=2){
+       classes.push('red')
+    }
+    if (this.state.persons.length <=1 ){
+       classes.push('bold')
+    }
+
     return (
-      <div className="App"> 
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style }
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App"> 
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style }
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
 
     // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now?'), 'Hi, I\'m a React App!!')
   }
 }
 
-export default App;
+export default Radium(App);
