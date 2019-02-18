2adc3a6480f99c90387737b412ec18e12edabff7
Lazer Porter <lazar.porter@gmail.com>
2019-02-18 16:05:56 -0500

    added some console logs to see how lifecycle hooks work

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('[App.js] constructor')
+  }
+
   state = {
     persons: [
       {id: 'adslfh', name: 'Chava', age: 2},
@@ -11,6 +16,15 @@ class App extends Component {
       {id: 'opkml', name: 'Lazer', age: 26}
     ],
     showPersons : false
+  }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
   }
 
  deletePersonHandler = (personIndex) =>{
@@ -42,6 +56,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render')
     let persons = null
 
     if (this.state.showPersons) {
