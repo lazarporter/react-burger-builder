685da836703d498e169e27c10468f903fdf749b8
Lazer Porter <lazar.porter@gmail.com>
2019-02-25 17:04:01 -0500

    lifecycle hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import classes from './Cockpit.css'
 
 const cockpit = (props) =>{
-    const assignedClasses = []
-    let btnClass = ''
-
-    if(props.showPersons){
-        btnClass = classes.red
-    }
+  useEffect( ()=>{
+    console.log('[Cockpit.js] useEffect')
     
-    if (props.persons.length <=2){
-      assignedClasses.push(classes.red)
-    }
-    if (props.persons.length <=1 ){
-      assignedClasses.push(classes.bold)
-    }
+  })
+  const assignedClasses = []
+  let btnClass = ''
 
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button
-                className={btnClass} 
-                onClick={props.click}>Toggle Persons</button>
-        </div>
-    )
+  if(props.showPersons){
+      btnClass = classes.red
+  }
+  
+  if (props.persons.length <=2){
+    assignedClasses.push(classes.red)
+  }
+  if (props.persons.length <=1 ){
+    assignedClasses.push(classes.bold)
+  }
+
+  return (
+      <div className={classes.Cockpit}>
+          <h1>{props.title}</h1>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button
+              className={btnClass} 
+              onClick={props.click}>Toggle Persons</button>
+      </div>
+  )
 }
 
 export default cockpit
--- src/containers/App.js
+++ src/containers/App.js
@@ -27,6 +27,13 @@ class App extends Component {
     console.log('[App.js] componentDidMount')
   }
 
+  shouldComponentUpdate(nextProps,nextState){
+    console.log('[App.js] shouldComponentUpdate')
+    return true
+  }
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate')
+  }
  deletePersonHandler = (personIndex) =>{
    const personsTemp = [...this.state.persons]
    personsTemp.splice(personIndex,1)
